a37ad0a2d362fa087a370c2111c9dd332bc417b0
Alisdair Walker <arwalker@gmail.com>
2012-04-22 17:05:01 -0400

    Added about box

--- src/com/arw/holoku/Holoku.java
+++ src/com/arw/holoku/Holoku.java
@@ -2,12 +2,40 @@ package com.arw.holoku;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Holoku extends Activity {
+public class Holoku extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Setup click listeners for all the buttons
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+
+    }
+    
+    public void onClick(View v)
+    {
+    	switch (v.getId())
+    	{
+    	case R.id.about_button:
+    		Intent i = new Intent(this, About.class);
+    		startActivity(i);
+    		break;
+    	}
     }
 }
